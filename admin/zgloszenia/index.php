2dace41f4363107d4470888cd47fe0d57842d78d
TomaszPis <tomek.pisarski@gmail.com>
2017-11-13 16:41:40 +0100

    Add files via upload

--- admin/zgloszenia/index.php
+++ admin/zgloszenia/index.php
@@ -1,1 +1,54 @@
+<?php
+  
+ session_start(); 
+  
+ require $_SERVER['DOCUMENT_ROOT'] . '/admin/function/access.php';
+
+
+ if (!userIsLoggedIn())
+{
+	include 'login.php';
+	exit();
+}
+ 
+ //Usuñ zg³oszenia
+
+ if (isset($_POST['delete']) == 'Usuñ')
+{ 
+	include $_SERVER['DOCUMENT_ROOT'] . '/admin/function/db.ini.php';
+	
+	$sql = "DELETE FROM zgloszenia WHERE id = '{$_POST['id']}'; ";
+	pg_query($sql);
+ 
+ header('Location: .');
+ exit();
+}
+
+ //Po³¹cz z baz¹ danych
+ include $_SERVER['DOCUMENT_ROOT'] . '/admin/function/db.ini.php';
+
+ //Wyœwietl zg³oszenia
+ try
+ {
+	 $sql = 'SELECT * FROM zgloszenia
+			ORDER BY id DESC;';
+	 $a = pg_query($sql);
+	 $b = pg_fetch_all($a);
+ }
+ catch(Exception $e)
+ {
+	 $error = 'B³¹d przy pobieraniu danych z bazy' . $e;
+	 include '../error.php';
+	 exit();
+ }
+ 
+ 
+ //za³aduj szablon
+ 
+include 'szablon.php';
+
+ 
+
+
+
 
---
+++ admin/zgloszenia/szablon.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<html lang="pl-PL">
+<title>Studio 2in1 - System zarządzania zgłoszeniami</title>
+<link rel="stylesheet" href="../../styles/admin.css" type="text/css">
+<link rel="stylesheet" href="../../styles/admin-480px.css" type="text/css">
+
+</head>
+<body>
+<header>
+	<h1>System zarządzania Zgłoszeniami</h1>
+	<!--- Menu -->
+	<nav>
+		<div class="link_box">
+			<a href="../">Home</a>
+		</div>
+		<div class="link_box">
+			<a href="../zgloszenia/">Wiadomości</a>
+		</div>
+		<div class="link_box">
+			<a href="../fotografia/">Fotografia</a>
+		</div>
+		<div class="link_box">
+			<a href="../blog/">Blog</a>
+		</div>
+		<?php include '../logout.php'; ?>
+	</nav>
+</header>
+<div id="contener">
+	<div id="message_box">
+				<?php foreach($b as $uslugi): ?>
+					 <div class="message">
+						<form action="" method="post">
+							<div class="info">
+							    <div class="w name">   Imię: </br><span class="color"> <?php echo $uslugi['imie']; ?></span>      	</div>
+								<div class="w service">Wybrana usługa: </br><span class="color"><?php echo $uslugi['usluga']; ?></span>		</div>
+								<div class="w mail">   E-mail: </br><span class="color"><?php echo $uslugi['mail']; ?></span>			</div>
+								<div class="w phone">  Telefon: <span class="color"></br><?php echo $uslugi['phone']; ?></span>			</div>
+								<div class="w zgloszenie"> Data zgłoszenia:</br><span class="color"><?php echo $uslugi['data_zgloszenia']; ?></span></div>
+								<div class="w delete">   
+											<input type="hidden" name="id" value="<?php echo $uslugi['id']; ?>" />
+											<input type="submit"  name="delete" value="Usuń" />
+								</div>
+							</div>
+							<div class="content">
+								<div class="w text">   <?php echo nl2br($uslugi['message_text']); ?>	</div>
+							</div>
+						</form>	
+					</div>
+				<?php endforeach; ?>
+	</div>
+	
