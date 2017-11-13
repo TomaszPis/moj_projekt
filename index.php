323105d7f855be9da4b8925ed48192225a8bdb04
TomaszPis <tomek.pisarski@gmail.com>
2017-11-13 16:27:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<html lang="pl-PL">
+<title>Studio 2in1</title>
+<meta name="keywords" content="fotografia, fotografia okolicznościowa, fotografia śluba, fotografia przedmałżeńska, fotografia reporterska, photography, wedding photography, sesje ślubne, sesje osiemnastkowe, sesje przedmałżeńskie, wesela, śluby, osiemnastki, imprezy okolicznościowe, zaproszenia, zaproszenia tradycyjne, zaproszenia internetowe, zaproszenia ślubne, zaproszenia urodzinowe, zaproszenia okolicznościowe, zaproszeni papierowe">
+<meta name="description" content="Portfolio młodych fotografów. Wejdź i sprawdź!">
+<link rel="stylesheet" href="styles/styles.css" type="text/css">
+
+</head>
+<body>
+<div id="start">
+	<a href=""><img src="images/<?php include $_SERVER['DOCUMENT_ROOT'] . '/szablony/logo.php' ?>" class="logo" alt="logo" /></a>
+	<p class="intro">Fotografia nie tylko okolicznościowa</p>
+	<ul>
+		<a href="fotografia/"><li>
+				<!-- <img src="images/fotografia.jpg" alt="intro" /> -->
+				<div class="name">
+					<p>Chcesz więcej?</p>
+				</div> 
+				<div class="name dwa">
+					<p>Kliknij</p>
+				</div>
+		</a></li>		
+
+		<!---<li>
+			<a href="zaproszenia/">
+				<img src="images/zaproszenia.jpg" alt="intro" />
+				<div class="name">
+					<p>Zaproszenia<p>
+				</div>
+			</a>
+		</li> -->
+	</ul>
+</div>
+<div id="footer">
+	<p>Strona w budowie</p>
+</div>
